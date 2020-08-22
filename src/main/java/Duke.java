a3b2b55ea7b9ebc3c1ed173eb386b98b381ab5ef
cococrunch <adinatatan99@gmail.com>
2020-08-22 23:56:39 +0800

    Add Level 2. Add, List Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,18 +3,35 @@ package main.java;
 import java.util.Scanner;
 
 public class Duke {
-    //echo user input
-    public static void echoInput(String userInput){
+    private static String[] listItems=new String[100];//user's to-do-list (max 100 items)
+    private static int listSize=0;//current list size
+    
+    //add input to list
+    public static void addList(String userInput){
+        listItems[listSize++]=userInput;
         System.out.printf(
             "____________________________________________________________\n" +
-            " %s\n" +
+            " added: %s\n" +
             "____________________________________________________________\n"
             ,userInput
         );
     }
     
+    //display current list
+    public static void displayList(){
+        System.out.printf(
+                "____________________________________________________________\n");
+        //print out all list's items
+        for (int i=0;i< listSize;i++){
+            System.out.printf(" %d. %s\n",i+1,listItems[i]);
+        }
+        System.out.printf(
+                "____________________________________________________________\n");
+    }
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         String userInput; //user input
         
         //greeting message
@@ -27,9 +44,15 @@ public class Duke {
         //scan user's input
         userInput = input.nextLine();
         
-        //echo user's input until "bye" is entered
+        //execute command based on user's input until "bye" is entered
         while(!userInput.equals("bye")){
-            echoInput(userInput);
+            //display list
+            if(userInput.equals("list")){
+                displayList();
+            }else {
+                //add user input to list
+                addList(userInput);
+            }
             userInput = input.nextLine();
         }
         
