ee2e77e9db91722fdee2a838daaf33898cca5503
cococrunch <adinatatan99@gmail.com>
2020-08-29 21:54:32 +0800

    A-CodingStandard: Follow the Coding Standard Tweak and refactor the code to comply with a given coding standard.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
     private static Task[] tasks=new Task[100];//user's to-do-list (max 100 items)
     private static int listSize=0;//current list size
     
-    //add input to list
+    //add user input to list
     public static void addList(String userInput){
         Task task = new Task(userInput);
         tasks[listSize++] = task;
@@ -21,8 +21,8 @@ public class Duke {
     //display current list
     public static void displayList(){
         System.out.print(
-                "____________________________________________________________\n"+
-                " Here are the tasks in your list:\n"
+            "____________________________________________________________\n"+
+            " Here are the tasks in your list:\n"
         );
         //print out all list's items
         for (int i = 0;i < listSize;i++){
@@ -32,44 +32,64 @@ public class Duke {
                 "____________________________________________________________\n");
     }
     
-    public static void main(String[] args) {
+    //handle user input commands
+    private static void handleCommand() {
         Scanner input = new Scanner(System.in);
-        
         String userInput; //user input
         
-        //greeting message
+        //scan user's input
+        userInput = input.nextLine();
+        
+        //execute command based on user's input until "bye" is entered
+        String userInput1 = userInput;
+        while(!userInput1.equals("bye")){
+            if(userInput1.equals("list")){
+                //display list
+                displayList();
+            } else if(userInput1.startsWith("done")){
+                //mark the given activity as done
+                markDone(userInput1);
+            } else {
+                //add user input to list
+                addList(userInput1);
+            }
+            //receive next user command
+            userInput1 = input.nextLine();
+        }
+    }
+    
+    //mark an activity as done
+    private static void markDone(String userInput1) {
+        //take word after "done" as task index
+        String taskIndexString = userInput1.split(" ")[1];
+        Integer taskIndex = Integer.parseInt(taskIndexString);
+        //mark task as done
+        tasks[taskIndex-1].markAsDone();
+    }
+    
+    //display bye message
+    private static void displayByeMessage() {
+        System.out.print(
+            "____________________________________________________________\n"+
+            " Bye. Hope to see you again soon!\n"+
+            "____________________________________________________________\n"
+        );
+    }
+    
+    //display greeting message
+    private static void displayGreetingMessage() {
         System.out.print(
             "____________________________________________________________\n" +
             " Hello! I'm Duke\n" +
             " What can I do for you?\n" +
             "____________________________________________________________\n"
         );
-        //scan user's input
-        userInput = input.nextLine();
-        
-        //execute command based on user's input until "bye" is entered
-        while(!userInput.equals("bye")){
-            //display list
-            if(userInput.equals("list")){
-                displayList();
-            } else if(userInput.startsWith("done")){
-                //take word after "done" as task index
-                String taskIndexString = userInput.split(" ")[1];
-                Integer taskIndex = Integer.parseInt(taskIndexString);
-                //mark task as done
-                tasks[taskIndex-1].markAsDone();
-            } else {
-                //add user input to list
-                addList(userInput);
-            }
-            userInput = input.nextLine();
-        }
-        
-        //bye message
-        System.out.print(
-            "____________________________________________________________\n"+
-            " Bye. Hope to see you again soon!\n"+
-            "____________________________________________________________\n"
-        );
+    }
+    
+    //main function
+    public static void main(String[] args) {
+        displayGreetingMessage();
+        handleCommand();
+        displayByeMessage();
     }
 }
